4b04a02f09f64c2431c6d283677497e844e53421
Xavier Valarino <xvalarino@gmail.com>
2020-11-03 16:39:52 -0800

    Add toggle for viewing the baseline

--- src/templates/debug.js
+++ src/templates/debug.js
@@ -1,7 +1,29 @@
+const body = document.querySelectorAll('body')
+const grids = document.querySelectorAll('.r-grid');
+
+function toggleTextContent (span) {   
+  span.textContent = span.textContent === 'OFF' ? 'ON' : 'OFF';
+
+}
+
+function toggleClasses (nodeList, className) {
+  Array.prototype.forEach.call(nodeList, el => {
+    el.classList.toggle(className);
+  });
+}
+
 document.querySelector('#debug-toggle')
   .addEventListener('click', function () {
-    const grids = document.querySelectorAll('.r-grid');
-    Array.prototype.forEach.call(grids, el => el.classList.toggle('debug'));
-    let readout = this.children[0];
-    readout.textContent = readout.textContent === 'OFF' ? 'ON' : 'OFF';
-  });
+    let span = this.querySelector('span');
+    toggleTextContent(span);
+    toggleClasses(grids, 'debug');
+  })
+;
+
+document.querySelector('#baseline-toggle')
+  .addEventListener('click', function () {
+    let span = this.querySelector('span');
+    toggleTextContent(span);
+    toggleClasses(body, 'baseline');
+  })
+;
