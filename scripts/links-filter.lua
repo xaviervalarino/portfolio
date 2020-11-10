abe4caa4440cf7475670bb25d65fbff003725bd5
Xavier Valarino <xvalarino@gmail.com>
2020-11-10 13:13:51 -0800

    Expand reach of links filter for local paths
    (Hopefully I don't regret this, but) the links filter isnt just for the
    image directory anymore. These changes convert *all* local paths to
    absolute ones.
    
    e.g.,
    ../path/name.txt --> /path/name.txt
    ./path/name.txt  --> /path/name.txt

--- scripts/links-filter.lua
+++ scripts/links-filter.lua
@@ -2,17 +2,17 @@ local function md_to_html (path)
   return string.gsub(path, "%.md$", ".html")
 end
 
-local function make_path_absolute (path)
+local function make_local_absolute (path)
   return string.gsub(path, "^%.-/-img/", "/img/")
 end 
 
 function Link (element)
   element.target = md_to_html (element.target)
-  element.target = make_path_absolute(element.target)
+  element.target = make_local_absolute(element.target)
   return element
 end 
 
 function Image (element)
-  element.src = make_path_absolute(element.src)
+  element.src = make_local_absolute(element.src)
   return element
 end
