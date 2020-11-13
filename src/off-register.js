4b470b64d9852c1737215873b7f9fcd33a966c1d
Xavier Valarino <xvalarino@gmail.com>
2020-11-13 12:33:03 -0800

    Fine tune off-registration effect
    Adds seven new frames,
    so instead of:
    s -> m -> l
    
    the animation classes travel from:
    1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9
    
    Also, the distance that these are triggered are now tied to the
    font-size (rem).

--- src/off-register.js
+++ src/off-register.js
@@ -1,38 +1,41 @@
 (function () {
+  const docElement = document.documentElement;
   const el = document.querySelector('.registration');
-  let lastScrollTop = 0;
-  let isScrolling;
-  let lastClass; 
+  // one actual rem for the site = .333v * 1 rem
+  // let, in case the window is resized
+  let rem = parseFloat(getComputedStyle(docElement).fontSize);
+  let lastClassId; 
 
-  function changeOffRegisterStyle (last, current) {
+  function getClassId (position) {
+    let classId; 
+    // .off-register class ids 1-9
+    for ( let i = 1; i < 10; i++ ) {
+      // +50: add a little bit of buffer 
+      let distance = rem * i + 50;
+      if ( position > distance ) {
+        classId = i
+      }
+    }
+    return classId;
+  }
+  function changeOffRegisterStyle (lastId, currentId) {
     requestAnimationFrame( () => {
-       if ( last) {
-         el.classList.remove(`off-register:${last}`)
+       if (lastId) {
+         el.classList.remove(`off-register:${lastId}`)
        }
-       if (current) {
-         el.classList.add(`off-register:${current}`)
+       if (currentId) {
+         el.classList.add(`off-register:${currentId}`)
        }
     });
-  return current;
+    return currentId;
   }
+
+  window.addEventListener("resize", () => {
+    rem = parseFloat(getComputedStyle(docElement).fontSize);
+  });
   window.addEventListener("scroll", () => { 
-    const position = window.pageYOffset || document.documentElement.scrollTop; 
-    const distance =  position - el.offsetTop; 
-    let currentClass; 
-    if ( position < 50 ) { currentClass = '' } 
-    else if ( position < 250 ) { currentClass = 's' } 
-    else if ( position < 450 ) { currentClass = 'm' } 
-    else { currentClass = 'l' } 
-    console.log(currentClass, position)
-    lastClass = changeOffRegisterStyle(lastClass, currentClass);
-
-    lastScrollTop = position <= 0 ? 0 : position; // For Mobile or negative scrolling
-
-    // isScrolling = setTimeout( () => {
-    //   console.log('timeout over')
-    //   lastClass = changeOffRegisterStyle(lastClass, '');
-    // }, 2000)
-}, false);
- 
-  window.on
+    const position = window.pageYOffset || docElement.scrollTop; 
+    let currentClassId = getClassId(position);
+    lastClassId = changeOffRegisterStyle(lastClassId, currentClassId);
+  }, false);
 } ())
