9cff9112960578623797658423e4374dd42982c6
Xavier Valarino <xvalarino@gmail.com>
2020-12-07 13:43:49 -0800

    Update masthead

---
+++ src/js/hamberder-menu.js
@@ -0,0 +1,10 @@
+// Hamberder
+(function(){
+  document.body.classList.add('with-js'); 
+  document.querySelector('.masthead button')
+    .addEventListener('click', function () {
+      let value = "true" === this.getAttribute('aria-expanded');
+      this.setAttribute('aria-expanded', !value)
+    })
+  ;
+}());
