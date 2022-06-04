db41f2dcf0b953443f56435be0c1f11e96f01044
Xavier Valarino <xvalarino@gmail.com>
2022-06-04 10:35:01 -0500

    Format `hamberder-menu` JS

--- src/js/hamberder-menu.js
+++ src/js/hamberder-menu.js
@@ -1,10 +1,10 @@
 // Hamberder
-(function(){
-  document.body.classList.add('with-js'); 
-  document.querySelector('.masthead button')
-    .addEventListener('click', function () {
-      let value = "true" === this.getAttribute('aria-expanded');
-      this.setAttribute('aria-expanded', !value);
-    })
-  ;
-}());
+(() => {
+  document.body.classList.add("with-js");
+  document
+    .querySelector(".masthead button")
+    .addEventListener("click", function () {
+      let value = "true" === this.getAttribute("aria-expanded");
+      this.setAttribute("aria-expanded", !value);
+    });
+})();
